0fd2ddea281c03bf55434e4b6ab9b9967e07d8d5
Maxim Kott <maximkott@gmail.com>
2021-07-09 22:28:17 +0200

    Replace microbundle with rollup

--- src/useQuery.spec.tsx
+++ src/useQuery.spec.tsx
@@ -1,14 +1,14 @@
 import { useQuery } from "./useQuery"
 import { MemoryRouter, Route } from "react-router"
 import React from "react"
-import { mount } from "enzyme"
-import { act } from "react-dom/test-utils"
+import { act, render, screen } from "@testing-library/react"
 
 describe("useQuery", () => {
   let receivedQuery: any
   let receivedSetQuery: any
   let receivedPatchQuery: any
   let renders = 0
+
   it("uses query", () => {
     const Test = () => {
       renders++
@@ -24,59 +24,58 @@ describe("useQuery", () => {
       )
     }
 
-    const wrapper = mount(
-      <MemoryRouter>
-        <Route component={Test} />
-      </MemoryRouter>
-    )
-    const target = wrapper.find("h1")
+    render(<MemoryRouter>
+      <Route component={Test} />
+    </MemoryRouter>)
+
+    const target = screen.getByRole("heading")
 
     expect(renders).toBe(1)
-    expect(target.text()).toBe("foo,baz")
+    expect(target).toHaveTextContent("foo,baz")
 
     act(() => receivedPatchQuery({ bar: "yolo" }))
 
     expect(renders).toBe(2)
-    expect(target.text()).toBe("foo,yolo")
+    expect(target).toHaveTextContent("foo,yolo")
 
     act(() => receivedSetQuery({ foo: "swag" }))
 
     expect(renders).toBe(3)
-    expect(target.text()).toBe("swag,baz")
+    expect(target).toHaveTextContent("swag,baz")
 
     act(() => receivedSetQuery({ foo: "swag" }))
 
     expect(renders).toBe(4)
-    expect(target.text()).toBe("swag,baz")
+    expect(target).toHaveTextContent("swag,baz")
 
     act(() => receivedPatchQuery({ bar: "" }))
 
     expect(renders).toBe(5)
-    expect(target.text()).toBe("swag,baz")
+    expect(target).toHaveTextContent("swag,baz")
 
     act(() => receivedPatchQuery({ bar: null }))
 
     expect(renders).toBe(6)
-    expect(target.text()).toBe("swag,baz")
+    expect(target).toHaveTextContent("swag,baz")
 
     act(() => receivedPatchQuery({ bar: undefined }))
 
     expect(renders).toBe(7)
-    expect(target.text()).toBe("swag,baz")
+    expect(target).toHaveTextContent("swag,baz")
 
     act(() => receivedPatchQuery({ bar: 0 }))
 
     expect(renders).toBe(8)
-    expect(target.text()).toBe("swag,baz")
+    expect(target).toHaveTextContent("swag,baz")
 
     act(() => receivedPatchQuery({ bar: "0" }))
 
     expect(renders).toBe(9)
-    expect(target.text()).toBe("swag,baz")
+    expect(target).toHaveTextContent("swag,baz")
 
     act(() => receivedPatchQuery({ bar: "x" }))
 
     expect(renders).toBe(10)
-    expect(target.text()).toBe("swag,x")
+    expect(target).toHaveTextContent("swag,x")
   })
 })
